d44fa3bd5495c83dd22ce7c9a2b03375f4bf536a
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-06-26 10:36:27 +0200

    Add book details page

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,39 @@ router.get("/books", (req, res, next) => {
     });
 });
 
+// To link here it's "/read?b=9999"
+router.get("/read", (req, res, next) => {
+  // Get the ID from the query string
+  // const b = req.query.b;
+  const { b } = req.query;
+
+  // Book.findOne({ _id: b })
+  Book.findById(b)
+    .then((bookDoc) => {
+      res.locals.bookItem = bookDoc;
+      res.render("book-details.hbs");
+    })
+    .catch((err) => {
+      // show our error page
+      next(err);
+    });
+});
+
+// To link here it's "/book/9999"
+router.get("/book/:bookId", (req, res, next) => {
+  // Get the ID from the URL
+  // const bookId = req.params.bookId
+  const { bookId } = req.params;
+
+  // Book.findOne({ _id: bookId })
+  Book.findById(bookId)
+    .then((bookDoc) => {
+      res.locals.bookItem = bookDoc;
+      res.render("book-details.hbs");
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
 module.exports = router;
