b9abbfbe6f0a10166052bd54caa0697d2174a98d
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-06-26 09:04:27 +0200

    Add list of books

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+
+const Book = require("../models/book-model.js");
+
 const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get("/books", (req, res, next) => {
+  Book.find()
+    .then((bookResults) => {
+      // send the database results to the view as "bookArray"
+      res.locals.bookArray = bookResults;
+      res.render("books-page.hbs");
+    })
+    .catch((err) => {
+      // show our error page
+      next(err);
+    });
 });
 
 module.exports = router;
