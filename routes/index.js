eec8a1e041f96d53f70fcdb9cf270a163882be44
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-06-26 15:20:06 +0200

    Create, Delete and Update

--- routes/index.js
+++ routes/index.js
@@ -40,6 +40,28 @@ router.get("/read", (req, res, next) => {
     });
 });
 
+// Place this route above "/book/:bookId" to avoid problems
+router.get("/book/add", (req, res, next) => {
+  res.render("book-form.hbs");
+});
+
+router.post("/process-book", (req, res, next) => {
+  // get variables from the form submission (req.body for POST)
+  const { title, author, description, rating } = req.body;
+
+  // save submission variables into our new book
+  Book.create({ title, author, description, rating })
+    .then((bookDoc) => {
+      // redirect ONLY to URLs ("/books" instead of "book-page.hbs")
+      // (redirect to avoid duplicate data on refresh)
+      res.redirect("/books");
+    })
+    .catch((err) => {
+      // show our error page
+      next(err);
+    });
+});
+
 // To link here it's "/book/9999"
 router.get("/book/:bookId", (req, res, next) => {
   // Get the ID from the URL
@@ -53,6 +75,65 @@ router.get("/book/:bookId", (req, res, next) => {
       res.render("book-details.hbs");
     })
     .catch((err) => {
+      // show our error page
+      next(err);
+    });
+});
+
+router.get("/book/:bookId/edit", (req, res, next) => {
+  // Get the ID from the URL
+  // const bookId = req.params.bookId
+  const { bookId } = req.params;
+
+  // Book.findOne({ _id: bookId })
+  Book.findById(bookId)
+    .then((bookDoc) => {
+      res.locals.bookItem = bookDoc;
+      res.render("book-edit.hbs");
+    })
+    .catch((err) => {
+      // show our error page
+      next(err);
+    });
+});
+
+router.post("/process-edit/:bookId", (req, res, next) => {
+  // Get the ID from the URL
+  // const bookId = req.params.bookId
+  const { bookId } = req.params;
+
+  // get variables from the form submission (req.body for POST)
+  const { title, author, description, rating } = req.body;
+
+  // Book.updateOne({ _id: bookId }, ...)
+  Book.findByIdAndUpdate(
+    bookId,
+    { $set: { title, author, description, rating } },
+    { runValidators: true }
+  )
+  .then((bookDoc) => {
+    // redirect ONLY to URLs ("/books/9999" instead of "book-details.hbs")
+    // (redirect to avoid duplicate data on refresh)
+    res.redirect(`/book/${bookId}`);
+  })
+  .catch((err) => {
+    // show our error page
+    next(err);
+  });
+});
+
+router.get("/book/:bookId/delete", (req, res, next) => {
+  // Get the ID from the URL
+  // const bookId = req.params.bookId
+  const { bookId } = req.params;
+
+  Book.findByIdAndRemove(bookId)
+    .then((bookDoc) => {
+      // redirect ONLY to URLs ("/books" instead of "book-page.hbs")
+      res.redirect("/books");
+    })
+    .catch((err) => {
+      // show our error page
       next(err);
     });
 });
